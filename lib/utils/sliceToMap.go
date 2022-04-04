7a2cbb9e81e91cf483ebf4bbbf4c5f1a59095148
Roniel Valdez <roniel_valdez@outlook.com>
2022-04-04 11:42:34 -0400

    Update lib/utils/sliceToMap.go
    Co-authored-by: Enrique Ortiz <hi@enriqueortiz.dev>

--- lib/utils/sliceToMap.go
+++ lib/utils/sliceToMap.go
@@ -1,7 +1,7 @@
 package utils
 
-// SliceToMappedStruct takes a slice and returns a string/T map with key being
-// a field of the struct
+// SliceToMappedStruct takes a slice and returns a string/T map with the key
+// being a field of the struct
 func SliceToMappedStruct[T any](slice []*T, fn func(*T) string) map[string]*T {
 	mapFromSlice := make(map[string]*T)
 	for _, value := range slice {
