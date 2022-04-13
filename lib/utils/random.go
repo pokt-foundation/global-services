79a08cfb41f2ce61c94d6a050c6dede08119f4b3
Roniel Valdez <roniel_valdez@outlook.com>
2022-04-13 13:10:09 -0400

    feat: random string generator

---
+++ lib/utils/random.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"encoding/hex"
+	"math/rand"
+)
+
+func RandomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
