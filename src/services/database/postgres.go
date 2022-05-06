1c37a991c17e05135c2003ec558330e5e6b2320c
Roniel Valdez <roniel_valdez@outlook.com>
2022-05-06 11:40:41 -0400

    Refactor database service for postgres support (#20)
    * chore: change mongo db name
    
    * feat: add postgres struct; init connection
    
    * refactor: use postgres struct
    
    * refactor: use postgres options for metrics db
    
    * docs: Add comment to public fields

---
+++ src/services/database/postgres.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+var (
+	// ErrValueUnderZero when a value given is under zero
+	ErrValueUnderZero = errors.New("value must be lower or equal to zero")
+	// ErrMaxPoolSizeOverMin when the value given for max pool size is over the min pool size
+	ErrMaxPoolSizeOverMin = errors.New("max pool size should be more than min pool size")
+)
+
+// PostgresOptions represents the options related to set up a pg connection
+type PostgresOptions struct {
+	Connection         string
+	MinMetricsPoolSize int
+	MaxMetricsPoolSize int
+}
+
+// Postgres is the struct for performing operations on a postgres database
+type Postgres struct {
+	Conn *pgxpool.Pool
+}
+
+// NewPostgresDatabase attempts and returns a postgres connection on success
+func NewPostgresDatabase(ctx context.Context, options *PostgresOptions) (*Postgres, error) {
+	if options.MinMetricsPoolSize < 0 || options.MaxMetricsPoolSize < 0 {
+		return nil, ErrValueUnderZero
+	}
+
+	if options.MinMetricsPoolSize > options.MaxMetricsPoolSize {
+		return nil, ErrMaxPoolSizeOverMin
+	}
+
+	config, err := pgxpool.ParseConfig(fmt.Sprintf("%s?pool_min_conns=%d&pool_max_conns=%d", options.Connection, options.MinMetricsPoolSize, options.MaxMetricsPoolSize))
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := pgxpool.ConnectConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Postgres{
+		Conn: conn,
+	}, nil
+}
