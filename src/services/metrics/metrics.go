243ae27442f249d07438e1e97674e08a781c094f
Roniel Valdez <roniel_valdez@outlook.com>
2022-05-06 11:34:35 -0400

    refactor: use postgres struct

--- src/services/metrics/metrics.go
+++ src/services/metrics/metrics.go
@@ -2,8 +2,8 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/Pocket/global-services/src/services/database"
 	"github.com/Pocket/global-services/src/services/logger"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
@@ -12,31 +12,26 @@ import (
 
 // Recorder represents the struct to write records to the metrics database
 type Recorder struct {
-	Conn *pgxpool.Pool
+	conn *pgxpool.Pool
 }
 
 // MetricsData Represents the
 
 // NewMetricsRecorder returns a connection to a metrics database
-func NewMetricsRecorder(ctx context.Context, connectionString string, minPoolSize, maxPoolSize int) (*Recorder, error) {
-	config, err := pgxpool.ParseConfig(fmt.Sprintf("%s?pool_min_conns=%d&pool_max_conns=%d", connectionString, minPoolSize, maxPoolSize))
+func NewMetricsRecorder(ctx context.Context, options *database.PostgresOptions) (*Recorder, error) {
+	postgres, err := database.NewPostgresDatabase(ctx, options)
 	if err != nil {
-		return nil, errors.New("unable to connect to database" + err.Error())
-	}
-
-	conn, err := pgxpool.ConnectConfig(ctx, config)
-	if err != nil {
-		return nil, errors.New("unable to connect to database: " + err.Error())
+		return nil, errors.New("unable to connect to metrics db: " + err.Error())
 	}
 
 	return &Recorder{
-		Conn: conn,
+		conn: postgres.Conn,
 	}, nil
 }
 
 // WriteErrorMetric writes an error metric to the connected database
-func (mr *Recorder) WriteErrorMetric(ctx context.Context, metric *Metric) {
-	_, err := mr.Conn.Exec(ctx, `
+func (r *Recorder) WriteErrorMetric(ctx context.Context, metric *Metric) {
+	_, err := r.conn.Exec(ctx, `
 	INSERT INTO
 	 error
 	 (timestamp,
@@ -67,4 +62,8 @@ func (mr *Recorder) WriteErrorMetric(ctx context.Context, metric *Metric) {
 			"appPublicKey": metric.TypeID,
 		}).Error("metrics: failure recording metric: " + err.Error())
 	}
+}
+
+func (r *Recorder) Close() {
+	r.conn.Close()
 }
