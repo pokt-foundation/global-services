ce5f5af01bf1c24dfdbdc4b13451e2cab7c1c1e1
Roniel Valdez <roniel_valdez@outlook.com>
2022-05-13 17:09:52 -0400

    feat: add math util

---
+++ shared/utils/math.go
@@ -0,0 +1,11 @@
+package utils
+
+import "golang.org/x/exp/constraints"
+
+func AvgOfSlice[T constraints.Integer | constraints.Float](slice []T) float64 {
+	total := 0.0
+	for _, elem := range slice {
+		total += float64(elem)
+	}
+	return total / float64(len(slice))
+}
