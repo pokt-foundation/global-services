285a130122436c5ec180efcb0c66b794d105125b
Roniel Valdez <roniel_valdez@outlook.com>
2022-05-13 17:10:06 -0400

    refactor: change naming

--- shared/utils/run-fn-on-slice-struct.go
+++ shared/utils/run-fn-on-slice-struct.go
@@ -5,14 +5,14 @@ import "golang.org/x/sync/errgroup"
 // RunFnOnSlice allows to run a function on all the elements of a
 // slice, having access to the individual elements of the slice,
 // returns err if any of them fails
-func RunFnOnSlice[T any](caches []*T, fn func(*T) error) error {
+func RunFnOnSlice[T any](slice []T, fn func(T) error) error {
 	var g errgroup.Group
-	for _, cacheClient := range caches {
-		func(ch *T) {
+	for _, element := range slice {
+		func(ch T) {
 			g.Go(func() error {
 				return fn(ch)
 			})
-		}(cacheClient)
+		}(element)
 	}
 	return g.Wait()
 }
